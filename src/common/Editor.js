1af430b8f77755b45fed9a0365a6bab92ecdbffe
David Chang <dchang.me@gmail.com>
2018-08-05 21:27:09 -0700

    using slate-md-editor, though it breaks BibleReferences/iframe replacing

--- src/common/Editor.js
+++ src/common/Editor.js
@@ -1,41 +1,21 @@
 import React, { Component } from 'react';
-import PasteLinkify from 'slate-paste-linkify';
 import AutoReplaceIframe from 'slate-auto-replace-iframe';
-import MarkHotkeys from 'slate-mark-hotkeys';
 import BibleReferences from 'slate-bible-references';
-import { Editor } from 'slate-react';
+import Editor from 'slate-md-editor';
 import isHotkey from 'is-hotkey';
+
+const MdEditor = Editor({});
 
 const isSaveHotkey = isHotkey('mod+s');
 
 const plugins = [
-  PasteLinkify({
-    type: 'link',
-    hrefProperty: 'url',
-    collapseTo: 'end',
-  }),
   AutoReplaceIframe(),
-  MarkHotkeys(),
   BibleReferences({
     esvAPIKey: '955cf1a9994ebdf2add173f69affb8814d3d8dd0',
   }),
 ];
 
 class CommonEditor extends Component {
-  renderNode = props => {
-    const { node, attributes, children } = props;
-    switch (node.type) {
-      case 'link':
-        return (
-          <a {...attributes} href={node.data.get('url')}>
-            {children}
-          </a>
-        );
-      default:
-        return null;
-    }
-  };
-
   onKeyDown = (event, change) => {
     const { onSave } = this.props;
     if (isSaveHotkey(event)) {
@@ -49,7 +29,7 @@ class CommonEditor extends Component {
     const { value, onChange } = this.props;
     return (
       <div className="EditorContainer">
-        <Editor
+        <MdEditor
           value={value}
           onChange={onChange}
           onKeyDown={this.onKeyDown}
